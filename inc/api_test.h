2e7f79cf7a23e7432b19be7c3ad67f93ace71186
zipper97412 <zipper97412@gmail.com>
2016-03-15 17:12:07 +0100

    makefuck

---
+++ inc/api_test.h
@@ -0,0 +1,12 @@
+#ifndef API_TEST_H
+#define API_TEST_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "map.h"
+
+int parser (char *buf, unsigned int len, char *search, void (*callback)(char* found, unsigned int len));
+void extract_stringL (StringL field, void (*callback)(char* found, unsigned int len));
+void affiche_string (char *found, unsigned len);
+
+#endif // API_TEST_H
---
+++ inc/map.h
@@ -0,0 +1,43 @@
+/**
+ * \file liste.h
+ * \brief Structure de la MAP
+ *
+ */
+
+#ifndef LISTE_H
+#define LISTE_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "StringL.h"
+
+/**
+ * \struct field
+ * \brief Maillon de la MAP du parseur
+ */
+typedef struct field field;
+struct field {
+    StringL field_name;     /*!< Type de header */
+    StringL header_field;   /*!< Contenu de ce header */
+    field* suivant;         /*!< Pointeur sur le prochain élément */
+};
+
+/**
+ * \struct mapStruct
+ * \brief Structure de la MAP
+ */
+typedef struct mapStruct mapStruct;
+struct mapStruct {
+    StringL methode;            /**< Pointeur vers le méthode de la requête */
+    StringL request_target;     /**< Pointeur vers la cible de la requête */
+    StringL http_version;       /**< Pointeur vers la version de http de la requête */
+    field* field;               /**< Pointeur sur la liste chainée contenant les header-fields */
+} ;
+
+mapStruct* init_map (StringL methode, StringL target, StringL version);
+int add_field (mapStruct* map, StringL header, StringL content);
+void search_map (mapStruct* map, char* search, void (*callback)(char* found, unsigned int len));
+void free_map (mapStruct* map);
+
+#endif
