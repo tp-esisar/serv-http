528d270879e3cfaaaf955a9e71b07a90a5f897c9
assombrance <assombrance.anderson@gmail.com>
2016-04-10 13:52:59 +0200

    ajout des header reader jusqu'à expires

--- src/parseur/reader.c
+++ src/parseur/reader.c
@@ -23,14 +23,18 @@ reader get_reader(syntaxe_elem se, StringL* wBuff) {
         case CRLF: return concat(letter('\r'),letter('\n')); //CRLF conforme (ne marche pas pour le debug avec l'encodage linux)
         //case CRLF: return or(letter('\n'),concat(letter('\r'),letter('\n'))); //CRLF non conforme à utiliser pour le debug sur linux
         case DIGIT: return charIn(((StringL){"0123456789",10}));
-        case day: return concat(symb(DIGIT),symb(DIGIT));
         case ALPHA: return charIn(((StringL){"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ",52}));
+        case HEXDIG: return or(symb(DIGIT), charIn(((StringL){"ABCDEFabcdef",6})));
+        case VCHAR: return charBetween(0x21, 0x7E);
+        case OCTET: return charBetween(0x00, 0xFF);
+        case BWS: return symb(OWS);
+        case DQUOTE: return letter('"');
+        case RWS: return nOccurencesMin(or(symb(SP),symb(HTAB)),1);
         case alphanum: return or(symb(DIGIT), symb(ALPHA));
         case unreserved: return or(symb(alphanum),charIn(((StringL){"-._~",4})));
         case tchar: return or(symb(alphanum),charIn(((StringL){"!#$\%&'*+-.^_`|~",15})));
         case token: return nOccurencesMin(symb(tchar),1);
         case field_name: return symb(token);
-        case VCHAR: return charBetween(0x21, 0x7E);
         case obs_text: return charBetween(0x80, 0xFF); 
         case field_vchar: return or(symb(VCHAR), symb(obs_text));
         //case field_content: return concat( symb(field_vchar), concat( nOccurencesMin(or(symb(SP), symb(HTAB)), 1), symb(field_vchar)));
@@ -40,7 +44,6 @@ reader get_reader(syntaxe_elem se, StringL* wBuff) {
         case field_value: return kleene(or(symb(field_content), symb(obs_fold)));
         case header_field: return concat( symb(field_name),concat( letter(':'), concat( symb(OWS), concat( symb(field_value), symb(OWS)))));
         case method: return symb(token);
-        case HEXDIG: return or(symb(DIGIT), charIn(((StringL){"ABCDEFabcdef",6})));
         case pct_encoded: return concat(letter('%'), concat(symb(HEXDIG), symb(HEXDIG)));
         case sub_delims: return charIn(((StringL){"!$&'()*+,;=",12}));
         case pchar: return or(symb(unreserved), or( symb(pct_encoded), or( symb(sub_delims), or( letter(':') , letter('@')))));
@@ -88,14 +91,13 @@ reader get_reader(syntaxe_elem se, StringL* wBuff) {
         case authority_form: return symb(authority);
         case asterisk_form: return letter('*');
         case request_target: return or(or( symb(origin_form), symb(absolute_form)), or( symb(authority_form), symb(asterisk_form)));
-        case HTTP_name: return concat(letter('H'),concat(letter('T'),concat(letter('T'),letter('P'))));
+        case HTTP_name: return concat(letter('H'),concat(letter('T'),concat(letter('T'),letter('P')))); // à remplacer par word_s("HTTP",4)
         case HTTP_version: return concat(concat(concat( symb(HTTP_name), letter('/')), symb(DIGIT)),concat( letter('.'), symb(DIGIT)));
         case request_line: return concat(concat(concat( symb(method), symb(SP)), concat( symb(request_target), symb(SP))),concat( symb(HTTP_version), symb(CRLF)));
         case status_code: return nOccurences(symb(DIGIT),3);
         case reason_phrase: return kleene(or(or( symb(HTAB), symb(SP)),or( symb(VCHAR), symb(obs_text))));
         case status_line: return concat(concat(concat( symb(HTTP_version), symb(SP)), concat( symb(status_code), symb(SP))),concat( symb(reason_phrase), symb(CRLF)));
         case start_line: return or(symb(request_line), symb(status_line));
-        case OCTET: return charBetween(0x00, 0xFF);
         case message_body: return kleene(symb(OCTET));
         case colon: return letter(':');//sale
         case connection_option: return symb(token);
@@ -105,20 +107,50 @@ reader get_reader(syntaxe_elem se, StringL* wBuff) {
         case Content_Length_header: return header("Content-Length:",15,Content_Length);
         case Trailer: return list(field_name);
         case Trailer_header: return header("Trailer:",8,Trailer);
-        case BWS: return symb(OWS);
         case qdtext: return or( or( or( symb(HTAB), symb(SP)), letter('!')), or( or( charBetween(0x23,0x5B), charBetween(0x5D,0x7E)), symb(obs_text)));
         case quoted_pair: return concat( letter('\\'), or( or( symb(HTAB), symb(SP)), or ( symb(VCHAR), symb(obs_text))));
-        case DQUOTE: return letter('"');
         case quoted_string: return concat( concat( symb(DQUOTE), kleene(or( symb(qdtext), symb(quoted_pair)))), symb(DQUOTE));
         case transfer_parameter: return concat(concat(concat( symb(token), symb(BWS)), letter('=')),concat( symb(BWS), or(symb(token), symb(quoted_string))));
         case transfer_extension: return concat( symb(token), kleene(concat( concat( symb(OWS), letter(';')), concat( symb(OWS), symb(transfer_parameter)))));
         case transfer_coding: return or( or( or( word_s("chunked",7), word_s("compress",8)), word_s("deflate",7)), or( word_s("gzip",4), symb(transfer_extension)));
         case Transfer_Encoding: return list(transfer_coding);
-        case Transfer_Encoding_header: return header("Transfer-Encoding", 17, Transfer_Encoding);
+        case Transfer_Encoding_header: return header("Transfer-Encoding:", 18, Transfer_Encoding);
         case protocol_name: return symb(token);
         case protocol_version: return symb(token);
         case protocol: return concat( symb(protocol_name), optionnal(concat( letter('/'), symb(protocol_version))));
         case Upgrade: return list(protocol);
+        case Upgrade_header: return header("Upgrade:",8,Upgrade);
+        case received_protocol: return concat(optionnal(concat(symb(protocol_name),letter('/'))),symb(protocol_version));
+        case pseudonym: return symb(token);
+        case uri_host: return symb(host);
+        case received_by: return or(concat(symb(uri_host),optionnal(concat(letter(':'),symb(port)))),symb(pseudonym));
+        case ctext: return or(or(or( symb(HTAB), symb(SP)), charBetween(0x21,0x27)), or(or( charBetween(0x2A,0x5B), charBetween(0x5D,0x7E)), symb(obs_text)));
+        case comment: return concat(concat( letter('('), kleene(or(or( symb(ctext), symb(quoted_pair)), symb(comment)))), letter(')'));
+        case Via: concat(concat( kleene(concat(letter(','), symb(OWS))), concat(concat( symb(received_protocol), symb(RWS)), concat( symb(received_by), optionnal(concat(symb(RWS), symb(comment)))))), kleene(concat(concat( symb(OWS), letter(',')), optionnal(concat(concat( symb(received_protocol), symb(RWS)), concat( symb(received_by), optionnal(concat(symb(RWS), symb(comment)))))))));
+        case Via_header: return header("Via:",4,Via);
+        case delta_seconds: return nOccurencesMin(symb(DIGIT),1);
+        case Age: return symb(delta_seconds);
+        case Age_header: return header("Age:",4,Age);
+        case day_name: return or(or(or( word_s("Mon",3), word_s("Tue",3)), or( word_s("Wed",3), word_s("Thu",3))), or(or( word_s("Fri",3), word_s("Sat",3)), word_s("Sun",3)));
+        case day: return nOccurences(symb(DIGIT),2);
+        case month: return or(or(or(or( word_s("Jan",3), word_s("Feb",3)),or( word_s("Mar",3), word_s("Apr",3))),or(or( word_s("May",3), word_s("Jun",3)),or( word_s("Jul",3), word_s("Aug",3)))),or(or( word_s("Sep",3), word_s("Oct",3)),or( word_s("Nov",3), word_s("Dec",3))));
+        case year: return nOccurences(symb(DIGIT),4);
+        case date1: return concat(concat(concat( symb(day), symb(SP)),concat( symb(month), symb(SP))), symb(year));
+        case hour: return nOccurences(symb(DIGIT),2);
+        case minute: return nOccurences(symb(DIGIT),2);
+        case second: return nOccurences(symb(DIGIT),2);
+        case time_of_day: return concat(concat(concat( symb(hour), letter(':')),concat( symb(minute), letter(':'))), symb(second));
+        case GMT: return word_s("GMT",3);
+        case IMF_fixdate: return concat(concat(concat( symb(day_name), letter(',')),concat( symb(SP), symb(date1))), concat(concat( symb(SP), symb(time_of_day)), concat( symb(SP), symb(GMT))));
+        case day_name_l: return or(or(or( word_s("Monday",6), word_s("Tuesday",7)), or( word_s("Wednesday",9), word_s("Thursday",8))), or(or( word_s("Friday",6), word_s("Saturday",8)), word_s("Sunday",6)));
+        case date2: return concat(concat(concat( symb(day), letter('-')),concat( symb(month), letter('-'))), nOccurences(symb(DIGIT),2));
+        case rfc850_date: return concat(concat(concat( symb(day_name_l), letter(',')),concat( symb(SP), symb(date2))), concat(concat( symb(SP), symb(time_of_day)), concat( symb(SP), symb(GMT))));
+        case date3: return concat(concat( symb(month), symb(SP)), or( nOccurences(symb(DIGIT),2), concat( symb(SP), symb(DIGIT))));
+        case asctime_date: return concat(concat(concat( symb(day_name), symb(SP)),concat( symb(date3), symb(SP))), concat(concat( symb(time_of_day), symb(SP)), symb(year)));
+        case obs_date: return or(symb(rfc850_date), symb(asctime_date));
+        case HTTP_date: return or(symb(IMF_fixdate),symb(obs_date));
+        case Expires: return symb(HTTP_date);
+        case Expires_header: return header("Expires:",8,Expires);
         default: return bad_symbole();
     }
 }
