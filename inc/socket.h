7173c55fede23ec97889cb26355ef39ac421af9a
bruchong <bruchong@esXXXX.esisar.grenoble-inp.fr>
2016-06-06 16:05:33 +0200

    youhou

---
+++ inc/socket.h
@@ -0,0 +1,18 @@
+#ifndef SOCKET_H
+#define SOCKET_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+#include <netinet/ip.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include "fcgi.h"
+
+int record_fcgi (char* adresse, int port, FCGI_Record_generic* message);
+FCGI_Record_generic* get_fcgi (int sock); //Ne pas oublier de free et de fermer la connection !!!
+
+#endif //SOCKET_H
